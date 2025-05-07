14a5abed311dfa3859c0a456e1a6a31edcc26721
felipewnoob <massinifelipe04@gmal.com>
2025-05-06 21:40:34 -0300

    atualizacao2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
+function credito(){
+
+}
+
+function debito(){
+
+}
   return (
     <>
-      
+      <h1>iMesada</h1>
+      <h2>Controlinho financeiro</h2>
+
+    <input type="valor" />
+
+    <button onClick={credito}>CREDITO!</button>
+    <button onClick={debito}>DEBITO!</button>
+
     </>
   )
 }
